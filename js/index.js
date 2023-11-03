6038acd6c31733df5de13b0db1e3120b2a14578b
Phil <philrsheridan@gmail.com>
2023-11-03 16:54:21 -0400

    get users working, get repos working, passing all tests

--- js/index.js
+++ js/index.js
@@ -10,17 +10,21 @@ const GETconfig = {
 
 function buildCard(userData) {
     const userList = document.getElementById('user-list');
-    img = document.createElement('img')
-    p = document.createElement('p')
+    h4 = document.createElement('h4');
+    img = document.createElement('img');
+    p = document.createElement('p');
 
+    h4.className = "user"
+    h4.textContent = `${userData.login}`;
     img.src = userData.avatar_url;
     img.alt = `${userData.login} image not found`;
-    img.style.height = "40%";
-    img.style.width = "40%";
-    p.textContent = `${userData.login}: ${userData.url}`;
+    img.style.height = "auto";
+    img.style.width = "20%";
+    p.textContent = `${userData.url}`;
 
-    userList.appendChild(img)
-    userList.appendChild(p)
+    userList.appendChild(h4);
+    userList.appendChild(img);
+    userList.appendChild(p);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -28,18 +32,41 @@ document.addEventListener('DOMContentLoaded', () => {
         event.preventDefault();
         userName = event.target.search.value;
         const getUserUrl = `https://api.github.com/search/users?q=${userName}`;
-        const getReposUrl = `https://api.github.com/users/${userName}/repo`;
 
         fetch(getUserUrl, GETconfig) 
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
             for(index in data.items) {
-                console.log(data.items[index]);
                 buildCard(data.items[index]);    
-            }
-        })
-    })
-})
+            };
+        });
+
+    });
+
+    document.addEventListener('click', (event) => {
+        if (event.target.className == "user") {
+            const reposUserName = event.target.textContent;
+            const getReposUrl = `https://api.github.com/users/${reposUserName}/repos`;
+
+            const ul = document.createElement('ul');
+            event.target.appendChild(ul);
+
+            fetch(getReposUrl, GETconfig)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(response) {
+                console.log(response);
+                for (repo in response) {
+                    const li = document.createElement('li');
+                    li.textContent = response[repo].name;
+                    li.href = response[repo].url;
+                    ul.appendChild(li);
+                }
+            });
+
+        };
+    });
+});
